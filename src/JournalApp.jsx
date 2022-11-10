ebb60b394e102007241fff17ab7bc3c6f93a39f2
Jesus Bolivar <j5rojo94@gmail.com>
2022-11-09 23:28:17 -0300

    Configuración de rutas

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { AppRouter } from './router/AppRouter'
 
 export const JournalApp = () => {
   return (
-    <>JournalApp</>
+    <>
+        <AppRouter />
+    </>
   )
 }
